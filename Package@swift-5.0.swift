44e5301da5e35d14083402ccc15c214c9ce393e3
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-05-27 17:05:51 +0900

    Update PathKit with 1.0.0

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.1"),
         .package(url: "https://github.com/benoit-pereira-da-silva/CommandLine.git", from: "4.0.0"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0")
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "FengNiaoKit", dependencies: ["Rainbow", "PathKit"]),
